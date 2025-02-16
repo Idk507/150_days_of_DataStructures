de2b1ee3475600b0526030eff29df913906af336
Dhanushkumar <93417785+Idk507@users.noreply.github.com>
2025-02-16 22:26:22 +0530

    Create Merge Strings Alternately.py

---
+++ Array/Merge Strings Alternately.py
@@ -0,0 +1,54 @@
+"""
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+"""
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        l1,l2 = 0,0 
+        c = 0
+        ans = ""
+        while l1 < len(word1) and l2 < len(word2):
+            if c%2 == 0:
+                ans += word1[l1]
+                l1+=1
+
+            elif c%2 != 0:
+                ans += word2[l2]
+                l2+=1
+            c+=1
+        if l1 < len(word1):
+            ans+=word1[l1:]
+        if l2 < len(word2):
+            ans += word2[l2:]
+        return ans
+
+        
