240c6db759ec18493268113f3bf8d2ef9dd0c684
Dhanushkumar <93417785+Idk507@users.noreply.github.com>
2025-09-17 21:31:28 +0530

    Create min_max.py

---
+++ Array/min_max.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+#User function Template for python3
+
+class Solution:
+    def get_min_max(self, arr):
+        arr.sort()
+        min = arr[0]
+        max = arr[-1]
+        return min ,max
+     
