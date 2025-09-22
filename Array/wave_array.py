014baea5aa69599756a67262419f5fdc808ae49f
Dhanushkumar <93417785+Idk507@users.noreply.github.com>
2025-09-22 21:28:45 +0530

    Create wave_array.py

---
+++ Array/wave_array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortInWave(self, arr):
+        # code here
+        n = len(arr)
+        if n == 1:
+            return arr
+        for i in range(0,n-1,2):
+            arr[i],arr[i+1] = arr[i+1],arr[i]
+        return arr
+        
