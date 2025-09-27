7afc8a93af722e9f41e6dd3926ae038cb7c9a089
Dhanushkumar <93417785+Idk507@users.noreply.github.com>
2025-09-27 15:18:45 +0530

    Create heap_sort.py

---
+++ Array/heap_sort.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def heapSort(self, arr):
+        def heapify(arr, n, i):
+            largest = i
+            left = 2 * i + 1
+            right = 2 * i + 2
+        
+            if left < n and arr[left] > arr[largest]:
+                largest = left
+            if right < n and arr[right] > arr[largest]:
+                largest = right
+        
+            if largest != i:
+                arr[i], arr[largest] = arr[largest], arr[i]  # swap
+                heapify(arr, n, largest)
+        def buildHeap(arr, n):
+            for i in range(n // 2 - 1, -1, -1):
+                heapify(arr, n, i)
+        n = len(arr)        
+        buildHeap(arr, n)
+
+        for i in range(n - 1, 0, -1):
+            arr[0], arr[i] = arr[i], arr[0]  # Swap root with last
+            heapify(arr, i, 0) 
+            
