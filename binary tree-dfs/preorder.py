5e2f04849f1f896d899200a274221121ae72635f
Dhanushkumar <93417785+Idk507@users.noreply.github.com>
2025-12-08 22:07:50 +0530

    Refactor preorder function into Solution class

--- binary tree-dfs/preorder.py
+++ binary tree-dfs/preorder.py
@@ -1,4 +1,16 @@
-def preorder(root):
-    if not root:
-        return []
-    return [root.data] + preorder(root.left) + preorder(root.right)
+'''
+# Node Class:
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    def preOrder(self, root):
+    # code here
+        if not root:
+            return []
+        return [root.data] + self.preOrder(root.left) + self.preOrder(root.right)
+        
