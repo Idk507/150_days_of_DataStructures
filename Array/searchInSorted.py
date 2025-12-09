e63c9a2c6c37972d30ad663cc0fa3e741ed43007
Dhanushkumar <93417785+Idk507@users.noreply.github.com>
2025-12-09 18:17:13 +0530

    Create searchInSorted.py

---
+++ Array/searchInSorted.py
@@ -0,0 +1,15 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    def searchInSorted(self,arr, k):
+        left, right = 0, len(arr) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == k:
+                return True
+            if arr[mid] < k:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
